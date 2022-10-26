620526bd8ff6e2be4f3c39a7f78a06b9758411b9
Juulinha <116567026+Juulinha@users.noreply.github.com>
2022-10-26 11:48:51 -0300

    Update Primeiro App.py

--- Primeiro App.py
+++ Primeiro App.py
@@ -3,7 +3,7 @@ nome = input("digite seu nome: ")
 idade = int(input("digite sua idade: "))
 altura = float(input("digite sua altura: "))
 peso = float(input("digite sua peso: "))
-IMC = peso/altura**2
+imc = peso/altura**2
 
 print("-" * 30)
 print("Os Dados coletados foram:")
@@ -11,4 +11,19 @@ print("nome: ", nome)
 print("idade: ", idade, " anos")
 print("altura: ", altura)
 print("peso: ", peso," quilos")
-print("IMC: ", IMC)
+print("IMC: ", imc)
+
+if imc < 16:
+    print("Magreza Grave")
+elif imc < 18.5:
+    print("Magreza Leve")
+elif imc < 24.9:
+    print("Peso Normal")
+elif imc < 29.9:
+    print("Excesso de peso")
+elif imc < 34.9:
+    print("Obesidade classe 1")
+elif imc < 39.9:
+    print("Obesidade classe 2")
+else: 
+    print("Obesidade classe 3")
